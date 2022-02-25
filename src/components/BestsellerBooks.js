d3f6363fdb6a1b8b49a7b34d13b6cbfc2c02f469
Triss2332 <beatricemelottiph@gmail.com>
2022-02-25 21:21:42 +0100

    prima fetch funzionante - best seller

---
+++ src/components/BestsellerBooks.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function BestsellerBooks() {
+    const [ books, setBooks ] = useState([]);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const res = await axios.get(
+                `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${process.env.REACT_APP_GRAVIT_API_KEY}`
+            )
+            setBooks(res.data.results);
+
+            console.log(res.data.results);
+        }
+
+        fetchBooks();
+    }, []);
+
+    return (
+        <div>
+            <h1>BESTSELLER BOOKS</h1>
+            <section>
+                { books.map((book) => {
+                    const {
+                        title, 
+                        description, 
+                        author, 
+                        publisher, 
+                        isbns
+                    } = book
+
+                    return (
+                        <article key={title}>
+                            <div>
+                                <h3>{title}</h3>
+                                <p>{description}</p>
+                                <p>{author}</p>
+                                <p>{publisher}</p>
+                                <div>{isbns.map((isbn) => {
+                                    const {
+                                        isbn10, 
+                                        isbn13
+                                    } = isbn
+                                    return (
+                                        <div key={isbn}>
+                                            <p>{isbn10}</p> 
+                                            <p>{isbn13}</p>
+                                        </div>
+                                    )
+                                })}</div>
+                            </div>
+                        </article>
+                    )
+                })}
+            </section>
+        </div>
+    );
+}
+
+
+export default BestsellerBooks;
